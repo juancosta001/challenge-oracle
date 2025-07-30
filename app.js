766caec737df962db4edbff88fac951e2e8c8e3f
juancosta001 <juanacostapereira9@gmail.com>
2025-07-29 21:45:07 -0300

    Se agrega el primer y segundo item del trello para el challenge

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigo(){
+    if (document.getElementById("amigo").value === "") {
+        alert("Por favor, ingresa un nombre de amigo.");
+        return;
+    } else{
+        amigos.push(document.getElementById("amigo").value);
+        let lista = document.getElementById("listaAmigos");
+        lista.innerHTML = "";
+        for (let i = 0; i < amigos.length; i++) {
+            lista.innerHTML += amigos[i];
+            if (i < amigos.length - 1) {
+            lista.innerHTML += ", ";
+            }
+        }
+        document.getElementById("amigo").value = "";
+    }
+    
+}
